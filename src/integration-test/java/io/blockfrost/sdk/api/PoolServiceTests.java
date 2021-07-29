ec77b3a52814fce0e89add69830e261bd58aeb86
Ss <cryptofervid@gmail.com>
2021-07-29 13:06:13 +1000

    Nested PoolServiceTests in groups.

--- src/integration-test/java/io/blockfrost/sdk/api/PoolServiceTests.java
+++ src/integration-test/java/io/blockfrost/sdk/api/PoolServiceTests.java
@@ -7,6 +7,8 @@ import io.blockfrost.sdk.api.util.OrderEnum;
 import io.blockfrost.sdk.impl.PoolServiceImpl;
 import io.blockfrost.sdk.impl.helper.ValidationHelper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -25,63 +27,100 @@ public class PoolServiceTests extends TestBase {
         poolService = new PoolServiceImpl(Constants.BLOCKFROST_TESTNET_URL, projectId);
     }
 
-    @Test
-    public void pools_willReturn_poolsForCountPageAndAscendingOrder() throws APIException {
+    @Nested
+    @DisplayName("GetPools Tests")
+    class GetPoolsTests {
 
-        String[] expectedPoolList = {
-                "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w",
-                "pool18kd2k7kqt9gje9y0azahww4dqak9azeeg8ayl0xl7dzewg70vlf",
-                "pool13dgxp4ph2ut5datuh5na4wy7hrnqgkj4fyvac3e8fzfqcc7qh0h",
-                "pool1wnf793xkgrw3s800tfdkkg3s3ddgxkucenahzs7490g4q0cpe0v",
-                "pool156gxlrk0e3phxadasa33yzk9e94wg7tv3au02jge8eanv9zc4ym"
-        };
+        @Test
+        public void pools_willReturn_poolsForCountPageAndAscendingOrder() throws APIException {
 
-        List<String> poolList = poolService.getPools(5, 1, OrderEnum.asc);
+            String[] expectedPoolList = {
+                    "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w",
+                    "pool18kd2k7kqt9gje9y0azahww4dqak9azeeg8ayl0xl7dzewg70vlf",
+                    "pool13dgxp4ph2ut5datuh5na4wy7hrnqgkj4fyvac3e8fzfqcc7qh0h",
+                    "pool1wnf793xkgrw3s800tfdkkg3s3ddgxkucenahzs7490g4q0cpe0v",
+                    "pool156gxlrk0e3phxadasa33yzk9e94wg7tv3au02jge8eanv9zc4ym"
+            };
 
-        assertThat(poolList, hasSize(5));
-        assertThat(poolList, contains(expectedPoolList));
+            List<String> poolList = poolService.getPools(5, 1, OrderEnum.asc);
+
+            assertThat(poolList, hasSize(5));
+            assertThat(poolList, contains(expectedPoolList));
+
+        }
+
+        @Test
+        public void pools_willReturn_poolsForCountPage() throws APIException {
+
+            String[] expectedPoolList = {
+                    "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w",
+                    "pool18kd2k7kqt9gje9y0azahww4dqak9azeeg8ayl0xl7dzewg70vlf",
+                    "pool13dgxp4ph2ut5datuh5na4wy7hrnqgkj4fyvac3e8fzfqcc7qh0h",
+                    "pool1wnf793xkgrw3s800tfdkkg3s3ddgxkucenahzs7490g4q0cpe0v",
+                    "pool156gxlrk0e3phxadasa33yzk9e94wg7tv3au02jge8eanv9zc4ym"
+            };
+
+            List<String> poolList = poolService.getPools(5, 1);
+
+            assertThat(poolList, hasSize(5));
+            assertThat(poolList, contains(expectedPoolList));
+
+        }
+
+        @Test
+        public void pools_willThrowAPIException_onCountGreaterThan100() {
+
+            Exception exception = assertThrows(APIException.class, () -> poolService.getPools(101, 1));
+            assertThat(exception.getMessage(), containsString(ValidationHelper.COUNT_VALIDATION_MESSAGE));
+
+        }
 
     }
 
-    @Test
-    public void pools_willReturn_poolsForCountPage() throws APIException {
+    @Nested
+    @DisplayName("GetRetiredPools Tests")
+    class GetRetiredPoolsTests {
 
-        String[] expectedPoolList = {
-                "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w",
-                "pool18kd2k7kqt9gje9y0azahww4dqak9azeeg8ayl0xl7dzewg70vlf",
-                "pool13dgxp4ph2ut5datuh5na4wy7hrnqgkj4fyvac3e8fzfqcc7qh0h",
-                "pool1wnf793xkgrw3s800tfdkkg3s3ddgxkucenahzs7490g4q0cpe0v",
-                "pool156gxlrk0e3phxadasa33yzk9e94wg7tv3au02jge8eanv9zc4ym"
-        };
+        @Test
+        public void retiredPools_willReturn_retiredPoolsForCountPageAndAscendingOrder() throws APIException {
 
-        List<String> poolList = poolService.getPools(5, 1);
+            List<PoolRetirementInfo> poolRetirementInfoList = Arrays.asList(
+                    PoolRetirementInfo.builder().poolId("pool126zlx7728y7xs08s8epg9qp393kyafy9rzr89g4qkvv4cv93zem").epoch(76).build(),
+                    PoolRetirementInfo.builder().poolId("pool16kc6ck4clmhg2aykwhkymnz2ypk54yuvk0txt3p6mrw05hrsj3a").epoch(76).build(),
+                    PoolRetirementInfo.builder().poolId("pool1y25deq9kldy9y9gfvrpw8zt05zsrfx84zjhugaxrx9ftvwdpua2").epoch(78).build()
+            );
 
-        assertThat(poolList, hasSize(5));
-        assertThat(poolList, contains(expectedPoolList));
+            List<PoolRetirementInfo> poolList = poolService.getRetiredPools(3, 1, OrderEnum.asc);
+
+            assertThat(poolList, hasSize(3));
+            assertThat(poolList, contains(poolRetirementInfoList.toArray()));
+        }
+
+        @Test
+        public void retiredPools_willReturn_retiredPoolsForCountPage() throws APIException {
+
+            List<PoolRetirementInfo> poolRetirementInfoList = Arrays.asList(
+                    PoolRetirementInfo.builder().poolId("pool126zlx7728y7xs08s8epg9qp393kyafy9rzr89g4qkvv4cv93zem").epoch(76).build(),
+                    PoolRetirementInfo.builder().poolId("pool16kc6ck4clmhg2aykwhkymnz2ypk54yuvk0txt3p6mrw05hrsj3a").epoch(76).build(),
+                    PoolRetirementInfo.builder().poolId("pool1y25deq9kldy9y9gfvrpw8zt05zsrfx84zjhugaxrx9ftvwdpua2").epoch(78).build()
+            );
+
+            List<PoolRetirementInfo> poolList = poolService.getRetiredPools(3, 1);
+
+            assertThat(poolList, hasSize(3));
+            assertThat(poolList, contains(poolRetirementInfoList.toArray()));
+        }
+
+        @Test
+        public void retiredPools_willThrowAPIException_onCountGreaterThan100() {
+
+            Exception exception = assertThrows(APIException.class, () -> poolService.getRetiredPools(101, 1));
+            assertThat(exception.getMessage(), containsString(ValidationHelper.COUNT_VALIDATION_MESSAGE));
+
+        }
 
     }
 
-    @Test
-    public void pools_willThrowAPIException_onCountGreaterThan100() {
 
-        Exception exception = assertThrows( APIException.class, () -> poolService.getPools(101, 1 ));
-        assertThat(exception.getMessage(), containsString(ValidationHelper.COUNT_VALIDATION_MESSAGE));
-
-    }
-
-    @Test
-    public void retiredPools_willReturn_retiredPoolsForCountPageAndAscendingOrder() throws APIException {
-
-        List<PoolRetirementInfo> poolRetirementInfoList = Arrays.asList(
-                                                            PoolRetirementInfo.builder().poolId("pool126zlx7728y7xs08s8epg9qp393kyafy9rzr89g4qkvv4cv93zem").epoch(76).build(),
-                                                            PoolRetirementInfo.builder().poolId("pool16kc6ck4clmhg2aykwhkymnz2ypk54yuvk0txt3p6mrw05hrsj3a").epoch(76).build(),
-                                                            PoolRetirementInfo.builder().poolId("pool1y25deq9kldy9y9gfvrpw8zt05zsrfx84zjhugaxrx9ftvwdpua2").epoch(78).build()
-                                                        );
-
-        List<PoolRetirementInfo> poolList = poolService.getRetiredPools(3, 1, OrderEnum.asc);
-
-        assertThat(poolList, hasSize(3));
-        assertThat(poolList, contains(poolRetirementInfoList.toArray()));
-    }
 
 }
