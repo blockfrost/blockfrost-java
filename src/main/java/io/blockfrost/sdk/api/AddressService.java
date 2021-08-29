8c2152a1a746f6c8ca95f92013f9fee20ef191ed
Ss <cryptofervid@gmail.com>
2021-08-29 23:55:31 +1000

    Added getTransactions method in Address API

--- src/main/java/io/blockfrost/sdk/api/AddressService.java
+++ src/main/java/io/blockfrost/sdk/api/AddressService.java
@@ -3,6 +3,7 @@ package io.blockfrost.sdk.api;
 import io.blockfrost.sdk.api.exception.APIException;
 import io.blockfrost.sdk.api.model.Address;
 import io.blockfrost.sdk.api.model.AddressTotal;
+import io.blockfrost.sdk.api.model.AddressTransaction;
 import io.blockfrost.sdk.api.model.AddressUtxo;
 import io.blockfrost.sdk.api.util.OrderEnum;
 
@@ -82,23 +83,45 @@ public interface AddressService {
      * @param to      The block number and optionally also index where (inclusive) to end the search for results, concatenated using colon. Has to be higher than or equal to &#x60;from&#x60; parameter.
      * @return List&lt;AddressTransaction&gt;
      */
-    /*
-    List<AddressTransaction> getAddressTransactions(String address, int count, int page, OrderEnum order) throws APIException;
 
-    List<AddressTransaction> getAddressTransactions(String address, int count, int page) throws APIException;
+    List<AddressTransaction> getAddressTransactions(String address, int count, int page, OrderEnum order, String from, String to) throws APIException;
 
-    List<AddressTransaction> getAddressTransactions(String address, OrderEnum order) throws APIException;
+    /**
+     * Address transactions
+     * Transactions on the address in ascending order from the point of view of the blockchain, not the page listing itself. By default, we return oldest first, newest last.
+     *
+     * @param address Bech32 address. (required)
+     * @param count   The numbers of pools per page.
+     * @param page    The page number for listing the results.
+     * @param from    The block number and optionally also index from which (inclusive) to start search for results, concatenated using colon. Has to be lower than or equal to &#x60;to&#x60; parameter.
+     * @param to      The block number and optionally also index where (inclusive) to end the search for results, concatenated using colon. Has to be higher than or equal to &#x60;from&#x60; parameter.
+     * @return List&lt;AddressTransaction&gt;
+     */
 
-    List<AddressTransaction> getAddressTransactions(String address) throws APIException;
+    List<AddressTransaction> getAddressTransactions(String address, int count, int page, String from, String to) throws APIException;
 
-    List<AddressTransaction> getAddressTransactionsInRange(String address, int count, int page, OrderEnum order, String from, String to) throws APIException;
+    /**
+     * Address transactions
+     * Transactions on the address.
+     *
+     * @param address Bech32 address. (required)
+     * @param order   The ordering of items from the point of view of the blockchain, not the page listing itself. By default, we return oldest first, newest last.
+     * @param from    The block number and optionally also index from which (inclusive) to start search for results, concatenated using colon. Has to be lower than or equal to &#x60;to&#x60; parameter.
+     * @param to      The block number and optionally also index where (inclusive) to end the search for results, concatenated using colon. Has to be higher than or equal to &#x60;from&#x60; parameter.
+     * @return List&lt;AddressTransaction&gt;
+     */
+    List<AddressTransaction> getAddressTransactions(String address, OrderEnum order, String from, String to) throws APIException;
 
-    List<AddressTransaction> getAddressTransactionsInRange(String address, int count, int page, String from, String to) throws APIException;
-
-    List<AddressTransaction> getAddressTransactionsInRange(String address, OrderEnum order, String from, String to) throws APIException;
-
-    List<AddressTransaction> getAddressTransactionsInRange(String address, String from, String to) throws APIException;
-    */
+    /**
+     * Address transactions
+     * Transactions on the address in ascending order from the point of view of the blockchain, not the page listing itself. By default, we return oldest first, newest last.
+     *
+     * @param address Bech32 address. (required)
+     * @param from    The block number and optionally also index from which (inclusive) to start search for results, concatenated using colon. Has to be lower than or equal to &#x60;to&#x60; parameter.
+     * @param to      The block number and optionally also index where (inclusive) to end the search for results, concatenated using colon. Has to be higher than or equal to &#x60;from&#x60; parameter.
+     * @return List&lt;AddressTransaction&gt;
+     */
+    List<AddressTransaction> getAddressTransactions(String address, String from, String to) throws APIException;
 
 
 }
