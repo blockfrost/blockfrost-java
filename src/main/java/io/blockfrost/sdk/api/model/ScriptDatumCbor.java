3121dbc5bbfec93d9e6ed89a83e4dafd19a72ce9
bentaye <bentaye@gmail.com>
2022-09-16 21:14:40 +0100

    squash me: Removed @Setter from model classes

--- src/main/java/io/blockfrost/sdk/api/model/ScriptDatumCbor.java
+++ src/main/java/io/blockfrost/sdk/api/model/ScriptDatumCbor.java
@@ -5,15 +5,15 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
  * ScriptDatumCbor
  */
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
